800f1be09112794d6639d9bbc94806ec26df8f90
James Naftel <james@yourbase.io>
2020-06-09 16:04:40 -0400

    added support for registies: pull, push, query repo tags

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/yourbase/narwhal
 require (
 	github.com/fsouza/go-dockerclient v1.6.0 // indirect
 	github.com/google/go-cmp v0.4.1
+	github.com/heroku/docker-registry-client v0.0.0-20190909225348-afc9e1acc3d5
 	github.com/johnewart/go-dockerclient v1.3.7
+	github.com/prometheus/common v0.10.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
 )
 
---
+++ registry.go
@@ -0,0 +1,53 @@
+package narwhal
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/heroku/docker-registry-client/registry"
+	log "github.com/sirupsen/logrus"
+)
+
+// ListTagsByRepo uses the OCI v2 interface to query a registry for the tags associated
+// with a registry.
+func ListTagsByRepo(ctx context.Context, registryURL, repo, username, password string) ([]string, error) {
+	log.Debugf("ListTagsByRepo: Registry (%s)", registryURL)
+	reg, err := registry.New(registryURL, username, password)
+	if err != nil {
+		return nil, fmt.Errorf("ListRepoTagsforRegistry connection error: %v", err)
+	}
+
+	return listTagsByRepo(ctx, reg, repo)
+}
+
+// ListTagsByRepoInsecure uses the OCI v2 interface to query a registry for the tags associated
+// with a registry.
+func ListTagsByRepoInsecure(ctx context.Context, registryURL, repo, username, password string) ([]string, error) {
+	log.Debugf("ListTagsByRepoInsecure: Registry (%s)", registryURL)
+	reg, err := registry.NewInsecure(registryURL, username, password)
+	if err != nil {
+		return nil, fmt.Errorf("ListTagsByRepoInsecure connection error: %v", err)
+	}
+
+	return listTagsByRepo(ctx, reg, repo)
+}
+
+func listTagsByRepo(ctx context.Context, reg *registry.Registry, repo string) ([]string, error) {
+	//TODO(james) what do with the ctx?
+	if deadline, ok := ctx.Deadline(); ok {
+		timeout := deadline.Sub(time.Now())
+		if timeout <= 0 {
+			// Deadline already exceeded
+			return nil, fmt.Errorf("listTagsByRepo exceeded deadline")
+		}
+		reg.Client.Timeout = timeout
+	}
+
+	tags, err := reg.Tags(repo)
+	if err != nil {
+		return nil, fmt.Errorf("listTagsByRepo Tags error: %v", err)
+	}
+	log.Debugf("listTagsByRepo: Found %d tags for repo %s", len(tags), repo)
+	return tags, nil
+}
