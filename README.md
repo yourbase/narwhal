e341fb3b67d796d74e118f33fab3cd71f52bd05e
glacials <qhiiyr@gmail.com>
2020-08-18 13:45:50 -0700

    Move README examples to Go examples

--- README.md
+++ README.md
@@ -4,41 +4,7 @@
 
 Narwhal is a Go API for Docker and Docker containers.
 
-## Examples
-
-### Create a client
-```go
-client := narwhal.DockerClient()
-```
-
-### Create a container
-```go
-id, err := narwhal.CreateContainer(ctx, client, io.Stdout, narwhal.ContainerDefinition{
-  Image: "ubuntu:latest",
-});
-```
-
-### Start a container
-```go
-err := narhwal.StartContainer(ctx, client, containerID)
-```
-
-### Copy files into a container
-```go
-err := narwhal.UploadFile(ctx, client, containerID, "/usr/local/bin", "/usr/local/bin/yb")
-```
-
-### Run commands inside a container
-```go
-err := narwhal.ExecShell(ctx, client, containerID, "echo 'Hello world!'", &narwhal.ExecShellOptions{})
-```
-
-### Squash an image
-```go
-err := narwhal.SquashImage(ctx, client, repo, tag)
-```
-
-### More
-See the [full API documentation][docs].
+For example usage see [pkg.go.dev][examples].
 
 [docs]: https://pkg.go.dev/github.com/yourbase/narwhal?tab=doc
+[examples]: https://pkg.go.dev/github.com/yourbase/narwhal?tab=doc#pkg-examples
---
+++ example_test.go
@@ -0,0 +1,105 @@
+package narwhal_test
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/docker/distribution/uuid"
+	"github.com/yourbase/narwhal"
+)
+
+var (
+	ctx    = context.Background()
+	client = narwhal.DockerClient()
+)
+
+func randomDefinition() *narwhal.ContainerDefinition {
+	return &narwhal.ContainerDefinition{
+		Label: uuid.Generate().String(),
+		Image: "ubuntu:latest",
+	}
+}
+
+func ExampleCreateContainer() {
+	containerID, err := narwhal.CreateContainer(ctx, client, os.Stdout, randomDefinition())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer narwhal.RemoveContainerAndVolumes(ctx, client, containerID)
+
+	err = narwhal.RemoveContainerAndVolumes(ctx, client, containerID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Container removed!")
+
+	// Output:
+	// Container removed!
+}
+
+func ExampleStartContainer() {
+	containerID, err := narwhal.CreateContainer(ctx, client, os.Stdout, randomDefinition())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer narwhal.RemoveContainerAndVolumes(ctx, client, containerID)
+
+	if err := narwhal.StartContainer(ctx, client, containerID); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if isRunning, err := narwhal.IsRunning(ctx, client, containerID); err != nil {
+		fmt.Println(err)
+		return
+	} else if isRunning {
+		fmt.Println("Container is running!")
+	}
+
+	// Output:
+	// Container is running!
+}
+
+func ExampleUploadFile() {
+	containerID, err := narwhal.CreateContainer(ctx, client, os.Stdout, randomDefinition())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer narwhal.RemoveContainerAndVolumes(ctx, client, containerID)
+
+	if err := narwhal.StartContainer(ctx, client, containerID); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if isRunning, err := narwhal.IsRunning(ctx, client, containerID); err != nil {
+		fmt.Println(err)
+	} else if isRunning {
+		fmt.Println("Container is running!")
+	}
+
+	f, err := ioutil.TempFile("./", "tmpfile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer os.Remove(f.Name())
+
+	if err := narwhal.UploadFile(ctx, client, containerID, "/tmp/tmpfile", f.Name()); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("File uploaded!")
+
+	// Output:
+	// Container is running!
+	// File uploaded!
+}
